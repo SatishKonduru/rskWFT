8f7cff61c3c2391e3bafac23082f516811c0d4ef
satish <satishkonduru0225@gmail.com>
2024-11-27 22:40:36 +0530

    tree shaking

--- src/app/public-pages/home/home.component.ts
+++ src/app/public-pages/home/home.component.ts
@@ -3,7 +3,7 @@ import { HeaderComponent } from '../header/header.component';
 
 @Component({
   selector: 'app-home',
-  imports: [HeaderComponent],
+  imports: [],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css',
 })
