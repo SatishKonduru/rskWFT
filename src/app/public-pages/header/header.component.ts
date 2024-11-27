5279542fac7c3ad20df92a81173de6fdf202a06f
satish <satishkonduru0225@gmail.com>
2024-11-27 16:49:37 +0530

    header updated

--- src/app/public-pages/header/header.component.ts
+++ src/app/public-pages/header/header.component.ts
@@ -1,11 +1,11 @@
 import { CommonModule } from '@angular/common';
-import { Component, ViewChild } from '@angular/core';
+import { AfterViewInit, Component, OnInit, ViewChild } from '@angular/core';
 import { MatMenuModule, MatMenuTrigger } from '@angular/material/menu';
 import { MatIconModule } from '@angular/material/icon';
 import { RouterModule } from '@angular/router';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatButtonModule } from '@angular/material/button';
-import { BreakpointObserver } from '@angular/cdk/layout';
+import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
 @Component({
   selector: 'app-header',
   imports: [
@@ -20,17 +20,25 @@ import { BreakpointObserver } from '@angular/cdk/layout';
   styleUrl: './header.component.css',
 })
 export class HeaderComponent {
-  @ViewChild(MatMenuTrigger) menuTrigger!: MatMenuTrigger;
+  menuOpen = false;
 
   constructor(private breakpointObserver: BreakpointObserver) {
     // Listen for changes in the viewport size
     this.breakpointObserver
-      .observe(['(min-width: 961px)'])
+      .observe([Breakpoints.Handset]) // Matches devices with max-width: 959px
       .subscribe((result) => {
-        if (result.matches && this.menuTrigger) {
-          // Close the menu if screen size exceeds 960px
-          this.menuTrigger.closeMenu();
+        if (!result.matches) {
+          // If the screen size is larger than 960px, close the menu
+          this.menuOpen = false;
         }
       });
+  }
+
+  toggleMenu() {
+    this.menuOpen = !this.menuOpen;
+  }
+
+  closeMenu() {
+    this.menuOpen = false;
   }
 }
