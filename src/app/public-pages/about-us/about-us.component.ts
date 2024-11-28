41d07bcb748e78b72f345e1d6eda0afd3daa4c92
satish <satishkonduru0225@gmail.com>
2024-11-28 19:02:58 +0530

    About us started

--- src/app/public-pages/about-us/about-us.component.ts
+++ src/app/public-pages/about-us/about-us.component.ts
@@ -1,11 +1,20 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+import * as AOS from 'aos';
 @Component({
   selector: 'app-about-us',
   imports: [],
   templateUrl: './about-us.component.html',
-  styleUrl: './about-us.component.css'
+  styleUrl: './about-us.component.css',
 })
-export class AboutUsComponent {
-
+export class AboutUsComponent implements OnInit {
+  ngOnInit(): void {
+    AOS.init({
+      duration: 1000, // Animation duration in milliseconds
+      offset: 100, // Trigger animation 100px before the element is visible
+      once: false,
+    });
+  }
+  ngAfterViewChecked() {
+    AOS.refresh(); // Refresh AOS to detect new elements
+  }
 }
